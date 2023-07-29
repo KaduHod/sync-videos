800f6174ec6c2e6b0cb59407cc2ac755ea4ed0ee
Carlos <carlosjr.ribas@gmail.com>
2023-07-29 12:07:00 -0300

    web socket server

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
 import "./config/global";
+import "./socket";
+import express from "express";
+import { engine } from "express-handlebars";
+
+const server = express();
+server.engine('.hbs', engine({ extname: ".hbs" }));
+server.set("view engine", ".hbs");
+server.set("views", "./public/views");
+server.use(express.static("./public/assets"))
+
+server.get("/", (req, res) => {
+    res.render("home")
+})
+
+const port = Number(process.env.APP_PORT ?? 8000);
+
+server.listen(port, () => {
+    console.log(`Web server running on port ${port}`)
+    console.log(`Web socket server running on port ${process.env.SOCKET_PORT}`)
+})
